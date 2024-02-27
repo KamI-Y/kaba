19f8d90f08c1c6902067a77972af2f3d1bc106a7
kamiji <kamiji.y.3@gmail.com>
2024-02-27 13:23:03 +0900

    bs2.py追加

---
+++ bs2.py
@@ -0,0 +1,14 @@
+import requests
+from bs4 import BeautifulSoup
+
+res=requests.get('https://joytas.net/kaba/')
+res.encoding=res.apparent_encoding
+#第1引数にhtml文字列,第2引数にパーサーを指定する。今回は追加ライブラリ不要な'html.parser'を指定
+soup=BeautifulSoup(res.text,'html.parser')
+
+#BeautifulSoupオブジェクトをprintにそのまま渡すと全文を表示する
+#print(soup)
+
+#タブで要素取得
+ele = soup.find ('title')
+print(ele.text)
